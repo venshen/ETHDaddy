33d7507b3ba339b514f6bff501a8462fbf2f26b1
bcMaster1 <2408297461@qq.com>
2023-05-08 01:35:46 +0800

    first commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,7 +11,28 @@ const tokens = (n) => {
 }
 
 async function main() {
+  // Setup accounts & variables
+  const [deployer] = await ethers.getSigners()
+  const NAME = "ETH Daddy"
+  const SYMBOL = "ETHD"
 
+  // Deploy contract
+  const ETHDaddy = await ethers.getContractFactory("ETHDaddy")
+  const ethDaddy = await ETHDaddy.deploy(NAME, SYMBOL)
+  await ethDaddy.deployed();
+
+  console.log(`Deployed Domain Contract at: ${ethDaddy.address}\n`)
+
+  // List 6 domains
+  const names = ["小明.eth", "小东.eth", "小花.eth", "张三.eth", "李四.eth", "王五.eth"]
+  const costs = [tokens(10), tokens(25), tokens(15), tokens(2.5), tokens(3), tokens(1)]
+
+  for (var i = 0; i < 6; i++) {
+    const transaction = await ethDaddy.connect(deployer).list(names[i], costs[i])
+    await transaction.wait()
+
+    console.log(`Listed Domain ${i + 1}: ${names[i]}`)
+  }
 }
 
 main().catch((error) => {
